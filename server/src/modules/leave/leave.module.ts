14bba6689e8c602c007b05739df9fdd2affcb728
Amarkumar30 <amar.kumar.career@gmail.com>
2026-04-08 18:16:51 +0530

    fix: Resolve LeaveService dependency injection issue
    - Import NotificationsModule in LeaveModule to provide NotificationsGateway
    - This resolves the UnknownDependenciesException on Render deployment
    - NotificationsGateway is now available for leave status update notifications

--- server/src/modules/leave/leave.module.ts
+++ server/src/modules/leave/leave.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { LeaveController } from './leave.controller';
 import { LeaveService } from './leave.service';
 import { PrismaModule } from '../../prisma/prisma.module';
+import { NotificationsModule } from '../notifications/notifications.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, NotificationsModule],
   controllers: [LeaveController],
   providers: [LeaveService],
   exports: [LeaveService],
